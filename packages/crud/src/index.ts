e7bbaf6fbbbd7a3274309315cb3921f4c8b850e5
Alistair Smith <alistairsmith01@gmail.com>
2021-05-17 10:57:40 +0100

    fix type error

--- packages/crud/src/index.ts
+++ packages/crud/src/index.ts
@@ -1,10 +1,14 @@
 import {PrismaClient} from '@prisma/client';
+import {add} from '@acme/example';
 
 const client = new PrismaClient();
+console.log(add(20, 20));
 
 void client.$connect().then(async () => {
 	const tenant = await client.tenant.create({
-		data: {},
+		data: {
+			name: 'Tenant name',
+		},
 	});
 
 	await client.box.create({
@@ -12,7 +16,7 @@ void client.$connect().then(async () => {
 			tenant: true,
 		},
 		data: {
-			title: 'WTF.',
+			title: 'Title here test',
 			tenant_id: tenant.id,
 		},
 	});
